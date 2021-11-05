c646e6f680dc0cc4e1a96c4c6f20d07aef5cfcb1
Evan Clayton Smith <evan@evansmith.tech>
2021-11-05 18:38:04 -0500

    Now can change useCommandLineArgs to true, to make code use cmd line args

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 /*
     Mackenzie Hamlett
+    Evan Smith
     Jacob Cox
     Zachary Kay
 
@@ -10,18 +11,26 @@
     - the file is taken in through the user entering the file path from the computer and not assuming the .txt file is added to the program file, I will text yall later and see which yall think is better, not hard to switch to the other.
 */
 import java.util.Scanner;
-
 public class App {
+    // Change this to true to enable command line args, and not prompt keyboard input
+    static boolean useCommandLineArgs = false; 
     public static void main(String[] args) throws Exception {
         String filePath;
-        Scanner keyboard = new Scanner(System.in);
         ScannerClass scanFile = new ScannerClass();
+        if (!useCommandLineArgs) {
+            Scanner keyboard = new Scanner(System.in);
 
-        // scan in the file using users FILE PATH FROM COMPUTER
-        System.out.println("Input file path: ");
-        filePath = keyboard.nextLine();
-        
+            // scan in the file using users FILE PATH FROM COMPUTER
+            System.out.println("Input file path: ");
+            filePath = keyboard.nextLine();
+            
+            keyboard.close();
+        } else {
+            filePath = args[0]; // Get the cmd line arg 
+            System.out.print(filePath);
+        }
         if (filePath.contains(".txt")) {
+            // Pass it to the actual scanner
             scanFile.scan_ScannerClass(filePath);
         } else {
             System.out.println("error.");
