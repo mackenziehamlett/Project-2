e15035a0b67fc891aa742e19055304113701fe70
Evan Smith <evan.c.smith@ttu.edu>
2021-11-10 16:00:33 -0600

    parserHelper skeleton, removed old debug code

--- Parser.java
+++ Parser.java
@@ -1,43 +1,83 @@
+import java.util.Arrays;
+
 public class Parser {
-/*
-Usage
-Parser parser = new Parser(...);
-parser.parse();
-*/
+    /*
+     * Usage Parser parser = new Parser(...); parser.parse();
+     */
     String[] tokenList;
     String[] valueList;
+
     // Constructor from strings
     public Parser(String tokenString, String valueString) {
-        this.tokenList = tokenString.split(", ");
+        this.tokenList = tokenString.split(", "); // "a, b, c" => [a, b, c]
         this.valueList = valueString.split(", ");
-        // System.out.print("Token List: ");
-        // for (String s : this.tokenList) {
-        //     System.out.println(s);
-            
-        // }
-        // System.out.print("Value List: ");
-        // for (String s : this.valueList) {
-        //     System.out.println(s);
-        // }
+
     }
+
     // Constructor from lists
     public Parser(String[] tokenList, String[] valueList) {
         this.tokenList = tokenList;
         this.valueList = valueList;
     }
+
     // ? TODO return type?
     // Just prints out stuff, recursively calls things based on the token value
     public void parse() {
-        // this.println("Workingln");
-        // this.print("Working");
+        // TODO print <program>
+        this.parseHelper(0, this.tokenList, this.valueList);
     }
 
+    // We have no counter variable, we are resizing tokenList and valueList to
+    // remove the first element from the next call
+    private void parseHelper(int depth, String[] tokenList, String[] valueList) {
+        this.indent(depth);
+        this.println("<stmt_list>");
 
-    /* 
-        If we want to write a string to a file, 
-        instead of (or in addition to) printing the XML tag,
-        we can just edit it here
-    */
+        this.indent(depth + 1);
+        this.println("<stmt>");
+
+        // This is the current <stmt> object basically
+        // TODO switch case logic here
+        // TODO .....
+        /*
+         * ... ...
+         */
+
+        // Now that we got the switch/case thing done, we go to the next call
+        // Call other parseHelper functions to do specific things, that need their own
+        // depth
+
+        // This is the next <stmt_list> object basically
+        if (tokenList.length != 0 && valueList.length != 0) {
+
+            this.parseHelper(depth + 1, 
+                    Arrays.copyOfRange(tokenList, 1, tokenList.length),
+                    Arrays.copyOfRange(valueList, 1, valueList.length));
+        } else {
+            // TODO we are done printing, all of the parseHelper functions will begin to
+            // indent back and print end tags
+            if (tokenList.length != valueList.length) {
+                //* This is debugging stuff, you can ignore it. I'm trying to see if some weird edge cases happen, 
+                //* eventually and I'd rather catch them instantly then debug
+                System.out.println("TokenList and valueList are not the same size during parseHelperCalls");
+                System.out.println("Is this okay/allowed????");
+                System.out.println("Depth: " + depth);
+                for (String s : this.tokenList) {
+                    System.out.print(s);
+                }
+                for (String s : this.valueList) {
+                    System.out.print(s);
+                }
+                System.exit(0);
+            }
+        }
+
+        this.indent(depth + 1);
+        this.println("</stmt>");
+        this.indent(depth);
+        this.println("</stmt_list>");
+
+    }
 
     /*
      * If we want to write a string to a file, instead of (or in addition to)
