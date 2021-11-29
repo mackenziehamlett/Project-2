894593e7250cc7650990b31eee3110ceff6fa744
Evan Smith <evan.c.smith@ttu.edu>
2021-11-29 14:30:21 -0600

    Size support

--- IDHashMap.java
+++ IDHashMap.java
@@ -2,17 +2,24 @@ import java.util.HashMap;
 
 public class IDHashMap {
     private HashMap<String, Object> ids = new HashMap<String, Object>();
-    public IDHashMap() {}
+    public IDHashMap() {
+        size = 0;
+    }
+
+    public int size;
 
     /// Used for creating a copy with preset data
     private IDHashMap(HashMap<String, Object> id) {
         this.ids = id;
+        this.size = id.size();
     }
 
     public void setToken(String id, Object reference) {
         ids.put(id, reference);
+        this.size++;
     }
 
+    
     public Object getToken(String id) {
         return ids.get(id);
     }
